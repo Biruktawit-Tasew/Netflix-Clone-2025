be9648c5198794188d0a178c4909cdb29337ba8d
Biruktawit-Tasew <birukti.tasew@gmail.com>
2025-02-25 15:04:33 -0300

    build

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Netflix-Clone-2025/",
+  base: "/Netflix-Clone/",
   plugins: [react()],
 });
